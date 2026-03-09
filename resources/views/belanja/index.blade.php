0c509f7a09bd3a1f844d50cb6cd5c930fa4df181
sdntomang03 <sdntomang03jkt@gmail.com>
2026-03-09 13:59:02 +0700

    fixed belanja

--- resources/views/belanja/index.blade.php
+++ resources/views/belanja/index.blade.php
@@ -197,6 +197,16 @@
                                                         d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                                 </svg>
                                             </a>
+                                            <a href="{{ route('belanja.edit', $belanja->id) }}"
+                                                class="text-gray-400 hover:text-blue-600 transition-colors"
+                                                title="Edit Transaksi">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                                    viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        stroke-width="2"
+                                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                                </svg>
+                                            </a>
 
                                             @if($belanja->status == 'posted')
                                             <a href="{{ route('surat.cetakpdf', $belanja->id) }}" title="Cetak PDF"
@@ -209,6 +219,7 @@
                                                         d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z" />
                                                 </svg>
                                             </a>
+
                                             @if ($belanja->korek->singkat === 'NARASUMBER')
 
                                             {{-- TOMBOL KELOLA JURNAL KEGIATAN --}}
@@ -252,16 +263,7 @@
                                                     </svg>
                                                 </button>
                                             </form>
-                                            <a href="{{ route('belanja.edit', $belanja->id) }}"
-                                                class="text-gray-400 hover:text-blue-600 transition-colors"
-                                                title="Edit Transaksi">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                                                    viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        stroke-width="2"
-                                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                                </svg>
-                                            </a>
+
                                             <form action="{{ route('belanja.destroy', $belanja->id) }}" method="POST"
                                                 id="delete-form-{{ $belanja->id }}">
                                                 @csrf
