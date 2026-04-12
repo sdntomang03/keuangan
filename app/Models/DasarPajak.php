48048173f9165aa1a29f0ef36e4580181be135ba
sdntomang03 <sdntomang03jkt@gmail.com>
2026-04-12 19:58:15 +0700

    fix

--- app/Models/DasarPajak.php
+++ app/Models/DasarPajak.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\FilterAnggaranAktif;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DasarPajak extends Model
 {
+use FilterAnggaranAktif;
     use HasFactory;
 
     protected $fillable = [
