5baa86c2e331ad43a39a14d613d188dbbd149bf8
sdntomang03 <sdntomang03jkt@gmail.com>
2026-04-29 11:16:02 +0700

    fixed

--- resources/views/rkas/cetak_laporan.blade.php
+++ resources/views/rkas/cetak_laporan.blade.php
@@ -8,34 +8,39 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            font-size: 12px;
+            font-size: 10px;
+            /* Ukuran font diperkecil untuk menghemat ruang */
             color: #000;
             margin: 0;
-            padding: 20px;
+            padding: 10px;
+            /* Padding dikurangi */
         }
 
         .header {
             text-align: center;
-            margin-bottom: 20px;
+            margin-bottom: 10px;
+            /* Jarak header dikurangi */
             border-bottom: 2px solid #000;
-            padding-bottom: 10px;
+            padding-bottom: 5px;
         }
 
         .header h2,
         .header h3 {
-            margin: 5px 0;
+            margin: 3px 0;
+            /* Margin heading dikurangi */
         }
 
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-bottom: 20px;
+            margin-bottom: 10px;
         }
 
         th,
         td {
             border: 1px solid #000;
-            padding: 6px 8px;
+            padding: 3px 5px;
+            /* Padding sel tabel diperkecil agar rapat */
             vertical-align: top;
         }
 
@@ -78,17 +83,17 @@
             background-color: #fff;
         }
 
-        /* Indentasi berjenjang */
+        /* Indentasi berjenjang disesuaikan dengan ukuran yang lebih kecil */
         .indent-1 {
-            padding-left: 20px !important;
+            padding-left: 15px !important;
         }
 
         .indent-2 {
-            padding-left: 35px !important;
+            padding-left: 25px !important;
         }
 
         .indent-3 {
-            padding-left: 50px !important;
+            padding-left: 35px !important;
         }
 
         @media print {
@@ -98,7 +103,8 @@
 
             @page {
                 size: A4 portrait;
-                margin: 1.5cm;
+                margin: 1cm;
+                /* Margin kertas diperkecil menjadi 1cm */
             }
 
             th {
@@ -118,6 +124,11 @@
 
             .row-rekening {
                 background-color: #e2efda !important;
+                -webkit-print-color-adjust: exact;
+            }
+
+            .row-total {
+                background-color: #f0f0f0 !important;
                 -webkit-print-color-adjust: exact;
             }
 
@@ -142,15 +153,13 @@
     <div class="header">
         <h2>RENCANA KEGIATAN DAN ANGGARAN SEKOLAH (RKAS)</h2>
         <h3>TAHUN ANGGARAN {{ $anggaran->tahun ?? date('Y') }}</h3>
-        <p style="margin-top: 5px; text-transform: uppercase;">{{ $anggaran->nama_anggaran ?? 'TIDAK
-            DIKETAHUI' }}</p>
+        <p style="margin-top: 5px; text-transform: uppercase;">{{ $anggaran->nama_anggaran ?? 'TIDAK DIKETAHUI' }}</p>
     </div>
 
     <table>
         <thead>
             <tr>
                 <th width="4%">No</th>
-
                 <th width="37%">Uraian / Keterangan</th>
                 <th width="8%">Vol</th>
                 <th width="8%">Satuan</th>
@@ -164,7 +173,6 @@
             {{-- LEVEL 1: KEGIATAN --}}
             <tr class="row-kegiatan">
                 <td class="text-center">{{ $no++ }}</td>
-
                 <td colspan="4">Kegiatan: {{ $keg->nama_kegiatan }}</td>
                 <td class="text-right font-bold">{{ number_format($keg->total_kegiatan, 0, ',', '.') }}</td>
             </tr>
@@ -181,7 +189,6 @@
             {{-- LEVEL 3: KODE REKENING --}}
             <tr class="row-rekening">
                 <td></td>
-
                 <td class="indent-2" colspan="4">Belanja: {{ $rek->nama_rekening }}</td>
                 <td class="text-right">{{ number_format($rek->total_rekening, 0, ',', '.') }}</td>
             </tr>
@@ -190,7 +197,6 @@
             @foreach ($rek->komponen as $komp)
             <tr class="row-komponen">
                 <td></td>
-
                 <td class="indent-3">
                     {{ $komp->namakomponen }}
                     @if(!empty($komp->spek)) <br><small><i>Spek: {{ $komp->spek }}</i></small> @endif
@@ -208,13 +214,13 @@
                 <td colspan="6" class="text-center">Tidak ada data RKAS untuk anggaran ini.</td>
             </tr>
             @endforelse
-        </tbody>
-        <tfoot>
-            <tr style="background-color: #f0f0f0; font-weight: bold;">
+
+            {{-- TOTAL KESELURUHAN dipindah ke dalam tbody agar hanya dirender di halaman terakhir --}}
+            <tr class="row-total" style="background-color: #f0f0f0; font-weight: bold;">
                 <td colspan="5" class="text-right" style="padding-right: 15px;">TOTAL KESELURUHAN</td>
                 <td class="text-right">{{ number_format($grandTotal, 0, ',', '.') }}</td>
             </tr>
-        </tfoot>
+        </tbody>
     </table>
 
 </body>
