5a5436d8902844e2abc949c7488774d0b4f15c3c
sdntomang03 <sdntomang03jkt@gmail.com>
2026-05-15 08:06:05 +0700

    fixed

--- app/Http/Controllers/NpdController.php
+++ app/Http/Controllers/NpdController.php
@@ -58,6 +58,7 @@ class NpdController extends Controller
                 $npdPending = Npd::where('sekolah_id', $sekolah->id)
                     ->where('idbl', $first->idbl)
                     ->where('kodeakun', $first->kodeakun)
+                    ->whereIn('bulan', $bulanArray)
                     ->whereNotIn('status', ['ditolak'])
                     ->sum('nilai_npd');
 
