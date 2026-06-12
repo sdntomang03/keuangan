e0652319c7a641d7abd0a2dcf27448dd4ab158c9
sdntomang03 <sdntomang03jkt@gmail.com>
2026-06-13 06:15:56 +0700

    fixed

--- resources/views/belanja/edit.blade.php
+++ resources/views/belanja/edit.blade.php
@@ -201,22 +201,37 @@
 
 
                                                 </td>
-                                                <td class="p-4 w-44">
-                                                    <div class="relative group">
-                                                        <input type="number" step="any"
-                                                            :name="'items[' + index + '][harga_satuan]'"
-                                                            x-model="item.harga_satuan" @input="calculateTotal()"
-                                                            class="w-full border-gray-200 rounded-lg text-right font-bold p-1 pr-8 focus:ring-amber-500 focus:border-amber-500 transition-all"
-                                                            placeholder="0">
+                                                <td class="p-4 w-56">
+                                                    <div class="flex items-center gap-2">
+                                                        <div class="relative group flex-1">
+                                                            <input type="number" step="any"
+                                                                :name="'items[' + index + '][harga_satuan]'"
+                                                                x-model="item.harga_satuan" @input="calculateTotal()"
+                                                                class="w-full border-gray-200 rounded-lg text-right font-bold p-1 pr-8 focus:ring-amber-500 focus:border-amber-500 transition-all"
+                                                                placeholder="0">
 
-                                                        <button type="button" @click="roundItem(index)"
-                                                            title="Bulatkan Harga"
-                                                            class="absolute right-1 top-1/2 -translate-y-1/2 p-1 text-gray-400 hover:text-amber-600 hover:bg-amber-50 rounded-md transition-all duration-200 active:rotate-180">
-                                                            <svg class="w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg"
-                                                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <button type="button" @click="roundItem(index)"
+                                                                title="Bulatkan Harga"
+                                                                class="absolute right-1 top-1/2 -translate-y-1/2 p-1 text-gray-400 hover:text-amber-600 hover:bg-amber-50 rounded-md transition-all duration-200 active:rotate-180">
+                                                                <svg class="w-3.5 h-3.5"
+                                                                    xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                    viewBox="0 0 24 24" stroke="currentColor">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                                        stroke-width="2.5"
+                                                                        d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                                                                </svg>
+                                                            </button>
+                                                        </div>
+
+                                                        <button type="button" @click="removeItem(index)"
+                                                            title="Hapus Komponen"
+                                                            class="flex-shrink-0 p-1.5 text-red-400 bg-red-50 hover:bg-red-500 hover:text-white rounded-lg transition-all duration-200 shadow-sm border border-red-100">
+                                                            <svg class="w-4 h-4" fill="none" stroke="currentColor"
+                                                                viewBox="0 0 24 24">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                                                    stroke-width="2.5"
-                                                                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                                                                    stroke-width="2"
+                                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                                                                </path>
                                                             </svg>
                                                         </button>
                                                     </div>
