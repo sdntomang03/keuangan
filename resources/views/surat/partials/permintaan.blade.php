43ebb2da0690449ba5c0c07f7d01816cd8655da1
sdntomang03 <sdntomang03jkt@gmail.com>
2026-07-11 06:30:24 +0700

    fixed

--- resources/views/surat/partials/permintaan.blade.php
+++ resources/views/surat/partials/permintaan.blade.php
@@ -39,7 +39,7 @@
                         <tbody>
                             <tr>
                                 <td style="width: 35px; vertical-align: top;">Yth.</td>
-                                <td style="vertical-align: top;">Direktur {{ $rekanan->nama_rekanan }}</>
+                                <td style="vertical-align: top;">Direktur {{ $rekanan->nama_rekanan }}
                                 </td>
                             </tr>
                             <tr>
