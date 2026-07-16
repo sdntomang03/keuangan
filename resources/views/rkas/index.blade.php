2c2c1d8b8bf5033135ff831c7e71872c9403ed12
sdntomang03 <sdntomang03jkt@gmail.com>
2026-07-16 15:11:31 +0700

    fixed

--- resources/views/rkas/index.blade.php
+++ resources/views/rkas/index.blade.php
@@ -194,7 +194,7 @@
                         rows += `
                             <tr class="hover:bg-gray-50 dark:hover:bg-gray-700 transition">
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-100">${index + 1}</td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-emerald-600 dark:text-emerald-400">${item.kodeakun ?? '-'}</td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-emerald-600 dark:text-emerald-400">${item.korek.singkat ?? '-'}</td>
                                 <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-100">${namaKomponen}</td>
                                 <td class="px-6 py-4 text-sm text-gray-900 dark:text-gray-100">${item.koefisien}</td>
                             </tr>
