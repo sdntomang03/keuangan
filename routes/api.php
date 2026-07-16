c56b307101606df1ca0189532349fa1e73b160ab
sdntomang03 <sdntomang03jkt@gmail.com>
2026-07-16 14:27:24 +0700

    fixed

--- routes/api.php
+++ routes/api.php
@@ -34,4 +34,4 @@ Route::middleware('auth:sanctum')->group(function () {
     // Rute Users dari langkah sebelumnya (sekarang terlindungi)
     Route::get('/users', [UserController::class, 'index']);
 });
-Route::get('/rkas-realisasi', [ApiJsonController::class, 'getRkas'])->name('api.getRkas');
+Route::get('/rkas-rkas', [ApiJsonController::class, 'getRkas'])->name('api.getRkas');
